cb85b1ea3d325471b35f31847d2a06f321848075
vfarcic <viktor@farcic.com>
2016-05-15 18:35:00 +0200

    Changed Ansible and Docker versions

--- bootstrap_ansible.sh
+++ bootstrap_ansible.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 echo "Installing Ansible..."
-#apt-get install -y software-properties-common
-#apt-add-repository ppa:ansible/ansible
-#apt-get update
-#apt-get install -y ansible
-apt-get install -y python-pip python-dev
-pip install ansible==1.9.1
+apt-get install -y software-properties-common
+apt-add-repository ppa:ansible/ansible
+apt-get update
+apt-get install -y ansible
+cp /vagrant/ansible/ansible.cfg /etc/ansible/ansible.cfg
